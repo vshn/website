fb03bec224a24835054e3aacd683c4e8454e6e89
Maksim Gorodov <silencer.web@gmail.com>
2020-09-07 11:04:29 +0500

    chore: add and set up Stylelint and Markdownlint, remove Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,8 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    node: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
+  extends: ['airbnb'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
