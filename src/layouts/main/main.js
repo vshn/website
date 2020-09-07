24ca2d0717460539458fcf7fc6cb01f97e3d4caf
Maksim Gorodov <silencer.web@gmail.com>
2020-09-07 12:36:03 +0500

    feat(layouts): add SEO to MainLayout

--- src/layouts/main/main.js
+++ src/layouts/main/main.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
+import SEO from 'components/shared/seo';
 import Header from 'components/shared/header';
 
-const MainLayout = ({ children }) => (
+const MainLayout = ({ seo, children }) => (
   <>
+    <SEO {...seo} />
     <Header />
     <main>{children}</main>
   </>
 );
 
+MainLayout.defaultProps = {
+  seo: {},
+};
+
 MainLayout.propTypes = {
+  seo: PropTypes.objectOf(PropTypes.any),
   children: PropTypes.node.isRequired,
 };
 
