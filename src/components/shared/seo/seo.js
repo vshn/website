d77669dc2dd9c7e837de2f9ec5069c634be5873b
Maksim Gorodov <silencer.web@gmail.com>
2020-09-07 12:23:53 +0500

    refactor(components): update meta tags in SEO

--- src/components/shared/seo/seo.js
+++ src/components/shared/seo/seo.js
@@ -2,18 +2,16 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
-import createMetaImagePath from 'utils/create-meta-image-path';
 
-const SEO = ({ data: { title, description, image, slug } = {}, facebook } = {}) => {
+const SEO = ({
+  title, description,
+}) => {
   const {
     site: {
       siteMetadata: {
         siteTitle,
         siteDescription,
-        siteUrl,
-        siteImage,
         siteLanguage,
-        authorTwitterAccount,
       },
     },
   } = useStaticQuery(graphql`
@@ -22,10 +20,7 @@ const SEO = ({ data: { title, description, image, slug } = {}, facebook } = {}) 
         siteMetadata {
           siteTitle
           siteDescription
-          siteUrl
-          siteImage
           siteLanguage
-          authorTwitterAccount
         }
       }
     }
@@ -33,9 +28,6 @@ const SEO = ({ data: { title, description, image, slug } = {}, facebook } = {}) 
 
   const currentTitle = title || siteTitle;
   const currentDescription = description || siteDescription;
-  const currentUrl = slug ? `${siteUrl}/${slug}` : siteUrl;
-  const currentImage = image || siteImage;
-  const currentImagePath = createMetaImagePath(currentImage, siteUrl);
 
   return (
     <Helmet
@@ -48,15 +40,11 @@ const SEO = ({ data: { title, description, image, slug } = {}, facebook } = {}) 
       {/* General */}
       <meta name="description" content={currentDescription} />
       {/* Open Graph */}
-      <meta property="og:url" content={currentUrl} />
       <meta property="og:title" content={currentTitle} />
       <meta property="og:description" content={currentDescription} />
-      <meta property="og:image" content={currentImagePath} />
-      <meta property="og:type" content="website" />
-      {facebook && <meta property="fb:app_id" content={facebook.appId} />}
-      {/* Twitter Card tags */}
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:creator" content={authorTwitterAccount} />
+      {/* Twitter */}
+      <meta name="twitter:title" content={currentTitle} />
+      <meta name="twitter:description" content={currentDescription} />
     </Helmet>
   );
 };
