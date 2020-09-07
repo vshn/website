da02633bb902bfbdf76ab11e70ccea71b20ca9ab
Maksim Gorodov <silencer.web@gmail.com>
2020-09-07 11:47:56 +0500

    refactor: clean up the project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import MainLayout from 'layouts/main';
-import Link from 'components/shared/link';
 
-const IndexPage = () => (
+const HomePage = () => (
   <MainLayout>
-    <div className="container">
-      <div className="columns">
-        <div className="column">1</div>
-        <div className="column">2</div>
-        <div className="column">3</div>
-      </div>
-
-      <Link to="/page-2">Page 2</Link>
-    </div>
+    HomePage
   </MainLayout>
 );
 
-export default IndexPage;
+export default HomePage;
