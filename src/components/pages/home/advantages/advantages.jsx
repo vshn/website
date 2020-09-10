247faacaae142e118ca019083d4fcb9bb6e47994
Maksim Gorodov <Silencer.Web@gmail.com>
2020-09-10 10:26:25 +0500

    feat(components): add Advantages for Home (#6)
    * fix(components): add font-weight to strong in Heading
    
    * feat(components): add Advantages for Home

---
+++ src/components/pages/home/advantages/advantages.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+
+import Heading from 'components/shared/heading';
+
+import ItemIcon1 from './images/item-icon-1.inline.svg';
+import ItemIcon2 from './images/item-icon-2.inline.svg';
+import ItemIcon3 from './images/item-icon-3.inline.svg';
+
+import styles from './advantages.module.scss';
+
+const cx = classNames.bind(styles);
+
+const icons = [
+  <ItemIcon1 />,
+  <ItemIcon2 />,
+  <ItemIcon3 />,
+];
+
+const Advantages = ({ title, description, items }) => (
+  <section className={cx('wrapper')}>
+    <div className="container">
+      <Heading className={cx('title')} tag="h2" size="sm" color="secondary">{title}</Heading>
+      <Heading className={cx('description')} tag="p" size="xl" innerHTML={description} />
+
+      <ul className={cx('items-wrapper')}>
+        {
+          items.map(({ title, description, url }, index) => (
+            <li className={cx('item')} key={index}>
+              <a className={cx('item-inner')} href={url}>
+                <span className={cx('item-number')}>{index + 1}</span>
+                {icons[index]}
+                <Heading className={cx('item-title')} tag="h3" size="lg">{title}</Heading>
+                <p className={cx('item-description')}>{description}</p>
+                <span className={cx('item-read-more')}>Read more</span>
+              </a>
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+  </section>
+);
+
+Advantages.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
+
+export default Advantages;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import MainLayout from 'layouts/main';
+
 import Hero from 'components/pages/home/hero';
+import Advantages from 'components/pages/home/advantages';
 
 const hero = {
   title: 'We run your <strong>application</strong>',
@@ -9,9 +11,32 @@ const hero = {
   buttonUrl: '/',
 };
 
+const advantages = {
+  title: 'Advantages',
+  description: '<strong>We are experts</strong> in Container, Kubernetes, Openshift',
+  items: [
+    {
+      title: 'Get Back Time',
+      description: 'Our automation accelerates development, deployment and operations processes',
+      url: '/',
+    },
+    {
+      title: 'Increase Security',
+      description: 'Our processes are ISO 27001 certified and we operate according to the Swiss banking standards',
+      url: '/',
+    },
+    {
+      title: 'Reduce Cost',
+      description: 'DevOps is the industrial revolution in the software industry',
+      url: '/',
+    },
+  ],
+};
+
 const HomePage = () => (
   <MainLayout>
     <Hero {...hero} />
+    <Advantages {...advantages} />
   </MainLayout>
 );
 
