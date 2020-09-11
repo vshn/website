453faf7df041ce98cd0ca7ebfb6777a08c31ba04
Maksim Gorodov <silencer.web@gmail.com>
2020-09-11 11:21:59 +0500

    build: add limit to url-loader for loading svg

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,6 +59,9 @@ module.exports = {
                 },
               ],
             },
+            urlLoaderOptions: {
+              limit: 512,
+            },
           },
         ],
       },
