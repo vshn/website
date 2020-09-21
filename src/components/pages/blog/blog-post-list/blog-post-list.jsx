3b557326c80c158936febe30f997d793f332c8e4
vannnyle@gmail.com <vannnyle@gmail.com>
2020-09-21 16:22:57 +0300

    feat(components): add illustration in Blog post list for BLog

--- src/components/pages/blog/blog-post-list/blog-post-list.jsx
+++ src/components/pages/blog/blog-post-list/blog-post-list.jsx
@@ -1,25 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import { graphql, useStaticQuery } from 'gatsby';
+import { graphql, useStaticQuery } from 'gatsby';
 import classNames from 'classnames/bind';
 
-// import GatsbyImage from 'gatsby-image';
+import GatsbyImage from 'gatsby-image';
 
 import styles from './blog-post-list.module.scss';
 import Item from './item';
 
 const cx = classNames.bind(styles);
 
-const BlogPostList = ({ items }) => (
-  <section className={cx('wrapper')}>
-    <div className={cx('container', 'inner')}>
-      <div className={cx('items-wrapper')}>
-        {items.map((item, index) => <Item {...item} key={index} />)}
+const BlogPostList = ({ items }) => {
+  const {
+    image: {
+      childImageSharp: { fluid: image },
+    },
+  } = useStaticQuery(graphql`
+    {
+      image: file(relativePath: { eq: "pages/blog/blog-post-list/image.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 410) {
+            ...GatsbyImageSharpFluid_withWebp_noBase64
+          }
+        }
+      }
+    }
+  `);
+  return (
+    <section className={cx('wrapper')}>
+      <div className={cx('container', 'inner')}>
+        <div className={cx('items-wrapper')}>
+          {items.map((item, index) => <Item {...item} key={index} />)}
+        </div>
+        <div className={cx('illustration')} aria-hidden>
+          <span className={cx('rectangle', 'rectangle-1')} />
+          <span className={cx('rectangle', 'rectangle-2')} />
+          <span className={cx('rectangle', 'rectangle-3')} />
+          <GatsbyImage className={cx('image')} fluid={image} />
+        </div>
       </div>
-      {/* <GatsbyImage className={cx('image')} fluid={image} /> */}
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 BlogPostList.propTypes = {
   items: PropTypes.arrayOf(
