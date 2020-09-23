4d7a53ee74dc734953567fad850fde5fc9a80160
vannnyle@gmail.com <vannnyle@gmail.com>
2020-09-23 20:43:01 +0300

    feat(components): add styles for columns in Content

--- src/components/pages/blog-post/content/content.jsx
+++ src/components/pages/blog-post/content/content.jsx
@@ -10,14 +10,24 @@ const cx = classNames.bind(styles);
 
 const Content = () => {
   const {
+    featuredImage: {
+      childImageSharp: { fluid: featuredImage },
+    },
     image: {
       childImageSharp: { fluid: image },
     },
   } = useStaticQuery(graphql`
     {
-      image: file(relativePath: { eq: "pages/blog-post/content/image.jpg" }) {
+      featuredImage: file(relativePath: { eq: "pages/blog-post/content/image.jpg" }) {
         childImageSharp {
           fluid(maxWidth: 1070) {
+            ...GatsbyImageSharpFluid_withWebp_noBase64
+          }
+        }
+      },
+      image: file(relativePath: { eq: "pages/blog-post/content/rectangle.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid_withWebp_noBase64
           }
         }
@@ -29,9 +39,14 @@ const Content = () => {
     <article className={cx('wrapper')}>
       <div className="container">
         <div className={cx('content')}>
-          <p>We’re very happy to announce Red Hat OpenShift 4 services on APPUiO!</p>
+          <p>
+            We’re very happy to announce Red Hat OpenShift 4 services on
+            {' '}
+            <a href="#">APPUiO</a>
+            !
+          </p>
           <figure className="wp-block-image">
-            <GatsbyImage className={cx('image')} fluid={image} />
+            <GatsbyImage className={cx('image')} fluid={featuredImage} />
             <figcaption>Working with ModSecurity rules</figcaption>
           </figure>
           <p>
@@ -40,8 +55,7 @@ const Content = () => {
             and OpenShift as a main pillar of APPUiO and VSHN services.
           </p>
           <Heading className={cx('title')} tag="h3" size="lg" color="primary">
-            OpenShift 4 on APPUiO
-            beta program
+            OpenShift 4 on APPUiO beta program
           </Heading>
           <p>
             To celebrate this milestone we’re happy to provide the community with time-limited
@@ -65,8 +79,7 @@ const Content = () => {
           </ul>
           <p>
             So the foundation for VSHN and APPUiO was set up which led to a strong & healthy
-            natural
-            growth through focus on what we do best.
+            natural growth through focus on what we do best.
           </p>
           <p>
             In 2020, we are still open & transparent and are embracing Open Source & partnerships,
@@ -91,9 +104,7 @@ const Content = () => {
               this issue at the Open Source Summit a few weeks ago.
             </li>
             <li>
-              <a
-                href="/"
-              >
+              <a href="#">
                 https://www.theregister.com/2020/06/30/hard_to_find_linux_maintainers_says_torvalds/
               </a>
             </li>
@@ -103,9 +114,7 @@ const Content = () => {
               MacBook users rejoice: “Ability to swap fn and ctrl keys on Apple keyboards.”
             </li>
             <li>
-              <a
-                href="/"
-              >
+              <a href="#">
                 https://www.theregister.com/2020/06/30/hard_to_find_linux_maintainers_says_torvalds/
               </a>
             </li>
@@ -120,27 +129,55 @@ const Content = () => {
             <p>
               In fall 2018, the management team went to Malta for a week-long retreat,
               where we focused on the challenges that came with our growth as well as the company
-              strategy
-              for the upcoming years. Among other topics, part of these discussions were the
-              individual
-              goals of the members of the management. This was when I first started to really think
-              about my own role in the company and what the things are that I enjoy doing and I find
-              interesting and what aspects of the job I would rather do without. As you can imagine
-              this is not something that is easily figured out in a few hours. At least for me it
-              was a
-              process lasting several months.
+              strategy for the upcoming years. Among other topics, part of these discussions were
+              the individual goals of the members of the management. This was when I first started
+              to really think about my own role in the company and what the things are that I enjoy
+              doing and I find interesting and what aspects of the job I would rather do without. As
+              you can imagine this is not something that is easily figured out in a few hours. At
+              least for me it was a process lasting several months.
             </p>
             <footer><cite>– André</cite></footer>
           </blockquote>
           <p>
             The new board and management structure will have a strong focus on PeopleOps, OrgDev
             and the refining of the strategy set by the board. André is happy to handover his
-            position
-            in the management to Marco Fretz who has been supporting the management in an advisory
-            capacity for some time. Marco started early 2016 as a system engineer at VSHN. From the
-            beginning he became more and more interested in organizational topics far beyond his
-            daily job and contributed with constructive ideas and concepts to the success of
-            VSHN.
+            position in the management to Marco Fretz who has been supporting the management in an
+            advisory capacity for some time. Marco started early 2016 as a system engineer at VSHN.
+            From the beginning he became more and more interested in organizational topics far
+            beyond his daily job and contributed with constructive ideas and concepts to the success
+            of VSHN.
+          </p>
+          <div className="wp-block-columns">
+            <div className="wp-block-column">
+              The new board and management structure will have a strong focus on PeopleOps, OrgDev
+              and the refining of the strategy set by the board. André is happy to handover his
+              position in the management to Marco Fretz who has been supporting the management in an
+              advisory capacity for some time. Marco started early 2016 as a system engineer at
+              VSHN. From the beginning he became more and more interested in organizational topics
+              far beyond his daily job and contributed with constructive ideas and concepts to the
+              success of VSHN.
+            </div>
+            <div className="wp-block-column">
+              <figure className="wp-block-image">
+                <GatsbyImage className={cx('image')} fluid={image} />
+                <figcaption>Working with ModSecurity rules</figcaption>
+              </figure>
+            </div>
+          </div>
+          <blockquote>
+            <p>
+              Being an “old” VSHNeer, knowing all the good and bad stories and having an open mind
+              I’m sure I can be a driver for the changes VSHN needs to overcome future challenges.
+              Together, we can improve things for us as VSHNeers and our customers likewise – and
+              maintain our great family-like culture we value that much.
+            </p>
+            <footer><cite>– Marco</cite></footer>
+          </blockquote>
+          <p>
+            As an open source company we’re used to collaborating over company borders, so much
+            that it is part of our openly documented company core values. For our OpenShift 4
+            services this means we’re documenting our support, architecture, and tooling in the
+            open, and are submitting contributions upstream.
           </p>
         </div>
       </div>
