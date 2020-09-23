43369477816691e338c3e8aaea612ff568fc44e5
vannnyle@gmail.com <vannnyle@gmail.com>
2020-09-23 16:34:58 +0300

    feat(components): add Author for Blog post

---
+++ src/components/pages/blog-post/author/author.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useStaticQuery, graphql } from 'gatsby';
+import GatsbyImage from 'gatsby-image';
+import classNames from 'classnames/bind';
+
+import Heading from 'components/shared/heading';
+import Link from 'components/shared/link';
+
+import styles from './author.module.scss';
+
+const cx = classNames.bind(styles);
+
+const Author = ({ name, email, phone, description }) => {
+  const {
+    image: {
+      childImageSharp: { fluid: image },
+    },
+  } = useStaticQuery(graphql`
+    {
+      image: file(relativePath: { eq: "pages/blog-post/author/markus-speth.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 100) {
+            ...GatsbyImageSharpFluid_withWebp_noBase64
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <article className={cx('wrapper')}>
+      <div className="container">
+        <div className={cx('items-wrapper')}>
+          <GatsbyImage className={cx('image')} fluid={image} />
+          <div className={cx('content')}>
+            <Heading className={cx('name')} tag="h3" size="lg">{name}</Heading>
+            <div className={cx('info')}>
+              <Link to={email}>{email}</Link>
+              <span className={cx('circle')} />
+              <Link to={phone}>{phone}</Link>
+            </div>
+            <p className={cx('description')}>{description}</p>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
+};
+Author.propTypes = {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  phone: PropTypes.number,
+  description: PropTypes.string.isRequired,
+};
+
+Author.defaultProps = {
+  phone: '+41 44 545 53 00',
+};
+
+export default Author;
--- src/pages/blog-post.jsx
+++ src/pages/blog-post.jsx
@@ -3,6 +3,7 @@ import MainLayout from 'layouts/main';
 
 import Hero from '../components/pages/blog-post/hero';
 import Content from '../components/pages/blog-post/content';
+import Author from '../components/pages/blog-post/author';
 
 const hero = {
   title: 'VSHN announces Red Hat OpenShift 4 services',
@@ -13,9 +14,17 @@ const hero = {
   date: new Date('2020-08-24'),
 };
 
+const author = {
+  name: 'Markus Speth',
+  email: 'markus.spethvshn@vshn.ch',
+  number: '+41 44 545 53 00',
+  description: 'Markus ist CMO bei der VSHN AG und kümmert sich um Marketingthemen und Partner.',
+};
+
 export default () => (
   <MainLayout>
     <Hero {...hero} />
     <Content />
+    <Author {...author} />
   </MainLayout>
 );
