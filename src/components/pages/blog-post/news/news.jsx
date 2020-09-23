456332062bca52bfe760a29be842905ea35ba46b
vannnyle@gmail.com <vannnyle@gmail.com>
2020-09-23 20:37:25 +0300

    feat(components): add News for Blog post

---
+++ src/components/pages/blog-post/news/news.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+
+import BlogPostsList from 'components/shared/blog-posts-list';
+
+import styles from './news.module.scss';
+
+const cx = classNames.bind(styles);
+
+const News = ({ title, items }) => (
+  <BlogPostsList className={cx('wrapper')} title={title} items={items} />
+);
+
+News.propTypes = {
+  title: PropTypes.string.isRequired,
+  items: PropTypes.arrayOf(PropTypes.shape({
+    tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+    title: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+  })).isRequired,
+};
+
+export default News;
--- src/pages/blog-post.jsx
+++ src/pages/blog-post.jsx
@@ -4,6 +4,7 @@ import MainLayout from 'layouts/main';
 import Hero from '../components/pages/blog-post/hero';
 import Content from '../components/pages/blog-post/content';
 import Author from '../components/pages/blog-post/author';
+import News from '../components/pages/blog-post/news';
 
 const hero = {
   title: 'VSHN announces Red Hat OpenShift 4 services',
@@ -21,10 +22,71 @@ const author = {
   description: 'Markus ist CMO bei der VSHN AG und kümmert sich um Marketingthemen und Partner.',
 };
 
+const news = {
+  title: 'More <strong>news</strong>',
+  items: [
+    {
+      tags: ['Technical', 'Project Syn'],
+      title: 'Second Beta Release of Project Syn Tools',
+      text: 'Without further ado, we’re announcing the release 0.2 of the Project Syn tools. Since the first public release mid-March this year (read more about it in First...',
+      url: '/',
+    },
+    {
+      tags: ['General', 'VSHNinternal'],
+      title: 'VSHN’s Corporate Governance 2020',
+      text: 'How time flies! Almost six years ago we founded VSHN. In the beginning, eight of us sat in an open-plan office. Decisions could be made quickly and easily in...',
+      url: '/',
+    },
+    {
+      tags: ['General', 'Press releases'],
+      title: 'Partnership VSHN & Rancher',
+      text: 'VSHN enters into a partnership with Rancher. VSHN has been working with Rancher since 2018 and is now an official Rancher Partner. With Managed Rancher...',
+      url: '/',
+    },
+    {
+      tags: ['Technical', 'Project Syn'],
+      title: 'Second Beta Release of Project Syn Tools',
+      text: 'Without further ado, we’re announcing the release 0.2 of the Project Syn tools. Since the first public release mid-March this year (read more about it in First...',
+      url: '/',
+    },
+    {
+      tags: ['General', 'VSHNinternal'],
+      title: 'VSHN’s Corporate Governance 2020',
+      text: 'How time flies! Almost six years ago we founded VSHN. In the beginning, eight of us sat in an open-plan office. Decisions could be made quickly and easily in...',
+      url: '/',
+    },
+    {
+      tags: ['General', 'Press releases'],
+      title: 'Partnership VSHN & Rancher',
+      text: 'VSHN enters into a partnership with Rancher. VSHN has been working with Rancher since 2018 and is now an official Rancher Partner. With Managed Rancher...',
+      url: '/',
+    },
+    {
+      tags: ['Technical', 'Project Syn'],
+      title: 'Second Beta Release of Project Syn Tools',
+      text: 'Without further ado, we’re announcing the release 0.2 of the Project Syn tools. Since the first public release mid-March this year (read more about it in First...',
+      url: '/',
+    },
+    {
+      tags: ['General', 'VSHNinternal'],
+      title: 'VSHN’s Corporate Governance 2020',
+      text: 'How time flies! Almost six years ago we founded VSHN. In the beginning, eight of us sat in an open-plan office. Decisions could be made quickly and easily in...',
+      url: '/',
+    },
+    {
+      tags: ['General', 'Press releases'],
+      title: 'Partnership VSHN & Rancher',
+      text: 'VSHN enters into a partnership with Rancher. VSHN has been working with Rancher since 2018 and is now an official Rancher Partner. With Managed Rancher...',
+      url: '/',
+    },
+  ],
+};
+
 export default () => (
   <MainLayout>
     <Hero {...hero} />
     <Content />
     <Author {...author} />
+    <News {...news} />
   </MainLayout>
 );
