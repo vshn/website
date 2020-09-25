202655966e1d4eb7b7fd96cbb5c025def7a1a9ef
Maksim Gorodov <silencer.web@gmail.com>
2020-09-25 14:59:29 +0500

    feat(components): add animation to Hero for Home

--- src/components/pages/home/hero/hero.jsx
+++ src/components/pages/home/hero/hero.jsx
@@ -1,35 +1,95 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 
 import Heading from 'components/shared/heading';
 import Button from 'components/shared/button';
+import useLottie from 'hooks/use-lottie';
 
 import shape1 from './images/shape-1.svg';
 import shape2 from './images/shape-2.svg';
-import illustration from './images/illustration.svg';
+
+import initialAnimationData from './data/initial-animation.json';
+import loopedAnimationData from './data/looped-animation.json';
 
 import styles from './hero.module.scss';
 
 const cx = classNames.bind(styles);
 
-const Hero = ({ title, description, buttonText, buttonUrl }) => (
-  <section className={cx('wrapper')}>
-    <div className="container">
-      <Heading className={cx('title')} innerHTML={title} />
-      <p className={cx('description')}>{description}</p>
-      <Button className={cx('button')} to={buttonUrl}>{buttonText}</Button>
+const Hero = ({ title, description, buttonText, buttonUrl }) => {
+  const [isInitialAnimationReady, setInitialAnimationReady] = useState(false);
+  const [isLoopAnimationReady, setLoopAnimationReady] = useState(false);
 
-      <img className={cx('shape-1')} src={shape1} alt="" aria-hidden />
-      <img className={cx('shape-2')} src={shape2} alt="" aria-hidden />
+  const [isInitialAnimationFinished, setIsInitialAnimationFinished] = useState(false);
 
-      <div className={cx('illustration-wrapper')} aria-hidden>
-        <img className={cx('illustration-shape')} src={shape1} alt="" />
-        <img src={illustration} alt="" />
+  const [initialAnimation, initialAnimationRef] = useLottie(
+    { animationData: initialAnimationData },
+    {
+      loaded_images() {
+        setInitialAnimationReady(true);
+      },
+      complete() {
+        setIsInitialAnimationFinished(true);
+      },
+    },
+  );
+
+  const [loopedAnimation, loopedAnimationRef] = useLottie(
+    {
+      animationData: loopedAnimationData,
+      loop: true,
+    },
+    {
+      loaded_images() {
+        setLoopAnimationReady(true);
+      },
+    },
+  );
+
+  useEffect(
+    () => {
+      if (initialAnimation && isInitialAnimationReady) {
+        initialAnimation.play();
+      }
+
+      if (loopedAnimation && isLoopAnimationReady && isInitialAnimationFinished) {
+        loopedAnimation.play();
+      }
+    },
+    [
+      initialAnimation,
+      isInitialAnimationReady,
+      loopedAnimation,
+      isLoopAnimationReady,
+      isInitialAnimationFinished,
+    ],
+  );
+
+  return (
+    <section className={cx('wrapper')}>
+      <div className="container">
+        <Heading className={cx('title')} innerHTML={title} />
+        <p className={cx('description')}>{description}</p>
+        <Button className={cx('button')} to={buttonUrl}>{buttonText}</Button>
+
+        <img className={cx('shape-1')} src={shape1} alt="" aria-hidden />
+        <img className={cx('shape-2')} src={shape2} alt="" aria-hidden />
+
+        <div className={cx('illustration-wrapper')} aria-hidden>
+          <img className={cx('illustration-shape')} src={shape1} alt="" />
+          <div
+            className={cx('illustration', { visible: !isInitialAnimationFinished && isInitialAnimationReady })}
+            ref={initialAnimationRef}
+          />
+          <div
+            className={cx('illustration', 'looped', { visible: isInitialAnimationFinished && isLoopAnimationReady })}
+            ref={loopedAnimationRef}
+          />
+        </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 Hero.propTypes = {
   title: PropTypes.string.isRequired,
