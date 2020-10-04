6e44d5d6c2010656a8d5610ed25c563bb4fd8031
Vanny Le <vannnyle@gmail.com>
2020-10-04 18:01:11 +0300

    refactor(styles): change tag article to link for post in sub-menu for Home page

--- src/components/shared/sub-menu/sub-menu.jsx
+++ src/components/shared/sub-menu/sub-menu.jsx
@@ -11,16 +11,16 @@ const cx = classNames.bind(styles);
 
 const SubMenu = ({ items }) => (
   <div className={cx('container', 'dropdown-wrapper')}>
-    <article className={cx('post')}>
+    <Link className={cx('post')} to="/">
       <Heading className={cx('post-title')} tag="h2" size="md" color="tertiary">Report DevOps in Switzerland 2020</Heading>
-      <Link className={cx('post-link')} to="/">Read more</Link>
+      <span className={cx('post-link')}>Read more</span>
       <span className={cx('rectangle', 'rectangle-1')} aria-hidden />
       <span className={cx('rectangle', 'rectangle-2')} aria-hidden />
       <span className={cx('rectangle', 'rectangle-3')} aria-hidden />
       <span className={cx('ellipse', 'ellipse-1')} aria-hidden />
       <span className={cx('ellipse', 'ellipse-2')} aria-hidden />
       <span className={cx('ellipse', 'ellipse-3')} aria-hidden />
-    </article>
+    </Link>
     <ul className={cx('list')}>
       {items.map((item, index) => (
         <li className={cx('item')} key={index}>
