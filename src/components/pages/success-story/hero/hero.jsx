8a227bd800a06e428d24253e0e9b9e9a039b4357
Vanny Le <vannnyle@gmail.com>
2020-10-13 18:29:29 +0300

    feat: add the Success story page

---
+++ src/components/pages/success-story/hero/hero.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+
+import Heading from 'components/shared/heading/';
+import circles from './images/circles.svg';
+
+import styles from './hero.module.scss';
+import Facts from '../facts';
+
+const cx = classNames.bind(styles);
+
+const Hero = ({ category, title, description, items }) => (
+  <div className={cx('wrapper')}>
+    <div className={cx('container', 'inner')}>
+      <span className={cx('category')}>{category}</span>
+      <Heading className={cx('title')} tag="h2" size="xxl" color="tertiary" innerHTML={title} />
+      <p className={cx('description')}>{description}</p>
+      <div className={cx('info')}>
+        <Facts items={items} />
+      </div>
+      <img className={cx('circles')} src={circles} alt="" aria-hidden />
+    </div>
+  </div>
+);
+
+Hero.propTypes = {
+  category: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  items: PropTypes.arrayOf.isRequired,
+};
+
+Hero.defaultProps = {
+
+};
+
+export default Hero;
---
+++ src/pages/success-story.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import MainLayout from 'layouts/main';
+import Contact from 'components/shared/contact';
+import Hero from 'components/pages/success-story/hero';
+
+const hero = {
+  category: 'Success stories',
+  title: 'Acrevis Bank & VSHN',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard',
+  items: [
+    'Acrevis Bank AG, a leading regional bank between Lake Constance and Lake Zurich',
+    'Acrevis Bank AG has decided to partner with VSHN after an intensive evaluation and detailing phase.',
+    'Together with Puzzle ITC, a technology platform based on APPUiO was developed for the areas of implementation, realisation and operation of IT platforms.',
+  ],
+};
+
+export default () => (
+  <MainLayout>
+    <Hero {...hero} />
+    <Contact />
+  </MainLayout>
+);
