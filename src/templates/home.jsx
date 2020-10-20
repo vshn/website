6ec7cd9b2a365efa8fcdee835780abfdcf4ce5db
Maksim Gorodov <silencer.web@gmail.com>
2020-10-20 16:45:22 +0500

    refactor(templates): remove comments from the template "Home"

--- src/templates/home.jsx
+++ src/templates/home.jsx
@@ -15,66 +15,6 @@ import Jobs from 'components/pages/home/jobs';
 import News from 'components/pages/home/news';
 import Report from 'components/pages/home/report';
 
-// const news = {
-//   title: 'Check <strong>latest news</strong>',
-//   items: [
-//     {
-//       categories: ['Technical', 'Project Syn'],
-//       title: 'Second Beta Release of Project Syn Tools',
-//       text: 'Without further ado, we’re announcing the release 0.2 of the Project Syn tools. Since the first public release mid-March this year (read more about it in First...',
-//       url: '/',
-//     },
-//     {
-//       categories: ['General', 'VSHNinternal'],
-//       title: 'VSHN’s Corporate Governance 2020',
-//       text: 'How time flies! Almost six years ago we founded VSHN. In the beginning, eight of us sat in an open-plan office. Decisions could be made quickly and easily in...',
-//       url: '/',
-//     },
-//     {
-//       categories: ['General', 'Press releases'],
-//       title: 'Partnership VSHN & Rancher',
-//       text: 'VSHN enters into a partnership with Rancher. VSHN has been working with Rancher since 2018 and is now an official Rancher Partner. With Managed Rancher...',
-//       url: '/',
-//     },
-//     {
-//       categories: ['Technical', 'Project Syn'],
-//       title: 'Second Beta Release of Project Syn Tools',
-//       text: 'Without further ado, we’re announcing the release 0.2 of the Project Syn tools. Since the first public release mid-March this year (read more about it in First...',
-//       url: '/',
-//     },
-//     {
-//       categories: ['General', 'VSHNinternal'],
-//       title: 'VSHN’s Corporate Governance 2020',
-//       text: 'How time flies! Almost six years ago we founded VSHN. In the beginning, eight of us sat in an open-plan office. Decisions could be made quickly and easily in...',
-//       url: '/',
-//     },
-//     {
-//       categories: ['General', 'Press releases'],
-//       title: 'Partnership VSHN & Rancher',
-//       text: 'VSHN enters into a partnership with Rancher. VSHN has been working with Rancher since 2018 and is now an official Rancher Partner. With Managed Rancher...',
-//       url: '/',
-//     },
-//     {
-//       categories: ['Technical', 'Project Syn'],
-//       title: 'Second Beta Release of Project Syn Tools',
-//       text: 'Without further ado, we’re announcing the release 0.2 of the Project Syn tools. Since the first public release mid-March this year (read more about it in First...',
-//       url: '/',
-//     },
-//     {
-//       categories: ['General', 'VSHNinternal'],
-//       title: 'VSHN’s Corporate Governance 2020',
-//       text: 'How time flies! Almost six years ago we founded VSHN. In the beginning, eight of us sat in an open-plan office. Decisions could be made quickly and easily in...',
-//       url: '/',
-//     },
-//     {
-//       categories: ['General', 'Press releases'],
-//       title: 'Partnership VSHN & Rancher',
-//       text: 'VSHN enters into a partnership with Rancher. VSHN has been working with Rancher since 2018 and is now an official Rancher Partner. With Managed Rancher...',
-//       url: '/',
-//     },
-//   ],
-// };
-
 export default ({ data: { wpPage: { seo, acf: data } }, pageContext: { locale } }) => (
   <MainLayout seo={seo}>
     <Hero {...data.hero} />
