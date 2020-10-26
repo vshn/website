c91cf3e73ba7e0eb939d865d1245e6f1238a7b8c
Vanny Le <vannnyle@gmail.com>
2020-10-26 19:43:20 +0200

    fix(styles): change image in Jobs for Home page

--- src/components/pages/home/jobs/jobs.jsx
+++ src/components/pages/home/jobs/jobs.jsx
@@ -1,56 +1,26 @@
 import classNames from 'classnames/bind';
-import { useStaticQuery, graphql } from 'gatsby';
-import GatsbyImage from 'gatsby-image';
 import PropTypes from 'prop-types';
 import React from 'react';
 
 import Button from 'components/shared/button';
 import Heading from 'components/shared/heading';
 
+import image from './images/image.svg';
 import styles from './jobs.module.scss';
 
 const cx = classNames.bind(styles);
 
-const Jobs = ({ title, description, buttonText, buttonUrl }) => {
-  const {
-    image: {
-      childImageSharp: { fluid: image },
-    },
-  } = useStaticQuery(graphql`
-    {
-      image: file(relativePath: { eq: "pages/home/jobs/image.jpg" }) {
-        childImageSharp {
-          fluid(maxWidth: 1920) {
-            ...GatsbyImageSharpFluid_withWebp_noBase64
-          }
-        }
-      }
-    }
-  `);
+const Jobs = ({ title, description, buttonText, buttonUrl }) => (
+  <section className={cx('wrapper')}>
+    <div className={cx('container', 'inner')}>
+      <Heading className={cx('title')} tag="h2" size="xl" color="tertiary" highlightedWordsColor="secondary" innerHTML={title} />
+      <p className={cx('description')}>{description}</p>
+      <Button to={buttonUrl}>{buttonText}</Button>
 
-  return (
-    <section className={cx('wrapper')}>
-      <div className={cx('container', 'inner')}>
-        <Heading className={cx('title')} tag="h2" size="xl" color="tertiary" highlightedWordsColor="secondary" innerHTML={title} />
-        <p className={cx('description')}>{description}</p>
-        <Button to={buttonUrl}>{buttonText}</Button>
-
-        <span className={cx('rectangle', 'rectangle-1')} aria-hidden />
-        <span className={cx('rectangle', 'rectangle-2')} aria-hidden />
-        <span className={cx('rectangle', 'rectangle-3')} aria-hidden />
-        <span className={cx('rectangle', 'rectangle-4')} aria-hidden />
-        <span className={cx('rectangle', 'rectangle-5')} aria-hidden />
-        <span className={cx('rectangle', 'rectangle-6')} aria-hidden />
-        <span className={cx('rectangle', 'rectangle-7')} aria-hidden />
-        <span className={cx('rectangle', 'rectangle-8')} aria-hidden />
-        <span className={cx('rectangle', 'rectangle-9')} aria-hidden />
-        <span className={cx('rectangle', 'rectangle-10')} aria-hidden />
-        <span className={cx('rectangle', 'rectangle-11')} aria-hidden />
-      </div>
-      <GatsbyImage className={cx('background-image')} fluid={image} />
-    </section>
-  );
-};
+    </div>
+    <img className={cx('image')} src={image} alt="" aria-hidden />
+  </section>
+);
 
 Jobs.propTypes = {
   title: PropTypes.string.isRequired,
