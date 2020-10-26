a6f39dbe0085d71a3c92fad6e3f2a7f08b36cbf3
Vanny Le <vannnyle@gmail.com>
2020-10-26 20:06:18 +0200

    fix(styles): change background-image in FeaturedPost for Blog page

--- src/components/pages/blog/featured-post/featured-post.jsx
+++ src/components/pages/blog/featured-post/featured-post.jsx
@@ -29,25 +29,6 @@ const FeaturedPost = ({ title, text, buttonText, buttonUrl, date }) => {
           <p className={cx('text')}>{text}</p>
           <Button size="sm" to={buttonUrl}>{buttonText}</Button>
         </div>
-
-        <span className={cx('rectangle', 'rectangle-1')} aria-hidden />
-        <span className={cx('rectangle', 'rectangle-2')} aria-hidden />
-        <span className={cx('rectangle', 'rectangle-3')} aria-hidden />
-        <span className={cx('rectangle', 'rectangle-4')} aria-hidden />
-        <span className={cx('rectangle', 'rectangle-5')} aria-hidden />
-        <span className={cx('rectangle', 'rectangle-6')} aria-hidden />
-        <span className={cx('rectangle', 'rectangle-7')} aria-hidden />
-        <span className={cx('rectangle', 'rectangle-8')} aria-hidden />
-        <span className={cx('ellipse', 'ellipse-1')} aria-hidden />
-        <span className={cx('ellipse', 'ellipse-2')} aria-hidden />
-        <span className={cx('ellipse', 'ellipse-3')} aria-hidden />
-        <span className={cx('ellipse', 'ellipse-4')} aria-hidden />
-        <span className={cx('ellipse', 'ellipse-5')} aria-hidden />
-        <span className={cx('ellipse', 'ellipse-6')} aria-hidden />
-        <span className={cx('ellipse', 'ellipse-7')} aria-hidden />
-        <span className={cx('ellipse', 'ellipse-8')} aria-hidden />
-        <span className={cx('ellipse', 'ellipse-9')} aria-hidden />
-        <span className={cx('ellipse', 'ellipse-10')} aria-hidden />
       </div>
 
       <img className={cx('background-image', 'lg-hidden')} src={backgroundImageLgUp} alt="" aria-hidden />
