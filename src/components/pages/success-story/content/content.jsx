abf2787d819b223e861bf3154da86b52d3efe5b5
Vanny Le <vannnyle@gmail.com>
2020-11-06 18:57:56 +0200

    fix(components): add Personal Card as lazy block for Success Story page

--- src/components/pages/success-story/content/content.jsx
+++ src/components/pages/success-story/content/content.jsx
@@ -5,56 +5,14 @@ import React from 'react';
 import Facts from '../facts';
 
 import styles from './content.module.scss';
-// import Interview from './interview';
+import 'components/lazy-blocks/personal-card/personal-card.scss';
 
 const cx = classNames.bind(styles);
 
-const Content = ({ acf: { facts } }) => (
+const Content = ({ content, acf: { facts } }) => (
   <section className={cx('wrapper')}>
     <div className="container">
-      <div className={cx('content')}>
-        <p>
-          The modern and OpenShift based platform will provide acrevis with a future-oriented
-          foundation for further digitisation initiatives and combine security, stability and
-          flexibility. It also enables acrevis to react quickly, easily and efficiently to customer
-          requests and new market requirements and to integrate them into the existing system
-          landscape. Thanks to the scalability of the new technology platform, future digitisation
-          projects can be realised faster, easier and more cost-efficient. It also distinguishes the
-          regional bank as an active player in the ongoing digitization of the financial sector.
-        </p>
-        <p>
-          By using APPUiO and by containerizing the application, the deployment of acrevis was
-          standardized. Due to the DevOps approach and the high degree of automation acrevis is now
-          much faster on the market and can roll out new products and features for its customers.
-          For acrevis it is clear: with VSHN, a competent partner was found who guarantees the
-          operation of a secure and stable infrastructure.
-        </p>
-        {/* <Interview {...interview} /> */}
-        <h3>About Acrevis Bank AG</h3>
-        <p>
-          <a href="/">Acrevis Bank AG</a>
-          , a leading regional bank between Lake Constance and Lake Zurich, is
-          positioning itself by systematically covering the market area and offering
-          segment-specific services. Its core competencies lie in the financing of private real
-          estate, investment advice, asset management and commercial business with SMEs, trade and
-          industry and the self-employed.
-
-        </p>
-        <h3>About VSHN – The DevOps Company</h3>
-        <p>
-          VSHN was founded with the intention to fundamentally shake up the hosting market. As a
-          lean startup, we focused on the operation of IT platforms through automation, agility and
-          a continuous improvement process. Completely independent of location and without our own
-          hardware, we operate extensive applications according to the DevOps principle agile and
-          24/7 on any infrastructure, so that software developers can concentrate on their business
-          and IT operations are relieved. VSHN (pronounced like “vision”) is the leading Swiss
-          partner for DevOps, Docker, Kubernetes, OpenShift, Rancher & 24/7 Cloud Operations. Learn
-          more on
-          {' '}
-          <a href="www.vshn.ch">www.vshn.ch</a>
-
-        </p>
-      </div>
+      <div className={cx('content')} dangerouslySetInnerHTML={{ __html: content }} />
       <div className={cx('info-wrapper')}>
         <Facts {...facts} />
       </div>
@@ -63,12 +21,7 @@ const Content = ({ acf: { facts } }) => (
 );
 
 Content.propTypes = {
-  // interview: PropTypes.shape({
-  //   avatar: PropTypes.node.isRequired,
-  //   name: PropTypes.string.isRequired,
-  //   position: PropTypes.string.isRequired,
-  //   byLine: PropTypes.string.isRequired,
-  // }).isRequired,
+  content: PropTypes.string.isRequired,
   acf: PropTypes.shape({
     facts: PropTypes.shape({
       title: PropTypes.string.isRequired,
--- src/templates/success-story.jsx
+++ src/templates/success-story.jsx
@@ -7,23 +7,6 @@ import Hero from 'components/pages/success-story/hero';
 import Contact from 'components/shared/contact';
 import MainLayout from 'layouts/main';
 
-const content = {
-  interview: {
-    avatar: ' ',
-    name: 'Christian Gentsch',
-    position: 'Member of the Executive Board acrevis Bank AG and CEO Finanz-Logistik AG',
-    byLine: 'The interview was conducted by Markus Speth, CMO VSHN – The DevOps Company.',
-  },
-  facts: {
-    title: 'Facts',
-    items: [
-      'Acrevis Bank AG, a leading regional bank between Lake Constance and Lake Zurich',
-      'Acrevis Bank AG has decided to partner with VSHN after an intensive evaluation and detailing phase.',
-      'Together with Puzzle ITC, a technology platform based on APPUiO was developed for the areas of implementation, realisation and operation of IT platforms.',
-    ],
-  },
-};
-
 export default ({ data: { seo, wpSuccessStory: data }, pageContext: { locale } }) => (
   <MainLayout seo={seo}>
     <Hero {...data} />
@@ -36,6 +19,7 @@ export const query = graphql`
   query($id: String!) {
     wpSuccessStory(id: { eq: $id }) {
       title
+      content
       acf {
         category
         description
