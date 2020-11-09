8cfbcad95ecd9395e6feefa042a9d41d0f34bdef
Vanny Le <vannnyle@gmail.com>
2020-11-09 11:46:11 +0200

    feat: add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
         background_color: '#4cc3ff',
         theme_color: '#4cc3ff',
         display: 'minimal-ui',
-        // icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
     },
     {
