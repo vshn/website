f2953b702e372a58f7c75e8774550332668d3f7e
Kirill Bolotsky <frontend@bolotsky.dev>
2020-11-17 14:45:21 +0500

    refactor(components): adjusted categories component to work with pagination

--- src/components/pages/blog/categories/categories.jsx
+++ src/components/pages/blog/categories/categories.jsx
@@ -3,33 +3,46 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 import Link from 'components/shared/link';
+import translations from 'i18n';
 
 import styles from './categories.module.scss';
 
 const cx = classNames.bind(styles);
 
-const Categories = ({ items, activeItemSlug }) => (
-  <div className={cx('wrapper')}>
-    <div className={cx('container', 'inner')}>
-      <div className={cx('items-wrapper')}>
-        {items.map(({ category: { name, uri: slug } }, index) => (
-          <Link className={cx('item', { active: activeItemSlug === slug })} to={slug} key={index}>
-            {name}
+const Categories = ({ locale, rootPath, categories, activeCategoryId }) => {
+  console.log(categories);
+  console.log(activeCategoryId);
+  return (
+    <div className={cx('wrapper')}>
+      <div className={cx('container', 'inner')}>
+        <div className={cx('items-wrapper')}>
+          <Link className={cx('item', { active: !activeCategoryId })} to={rootPath}>
+            {translations[locale].blog.allPostsCategoryName}
           </Link>
-        ))}
+          {categories.map(({ name, slug, id }) => (
+            <Link className={cx('item', { active: activeCategoryId === id })} to={`${rootPath}${slug}`} key={id}>
+              {name}
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 Categories.propTypes = {
-  items: PropTypes.arrayOf(PropTypes.shape({
-    category: PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      uri: PropTypes.string.isRequired,
-    }).isRequired,
-  })).isRequired,
-  activeItemSlug: PropTypes.string.isRequired,
+  categories: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    slug: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+  }).isRequired).isRequired,
+  activeCategoryId: PropTypes.string,
+  rootPath: PropTypes.string.isRequired,
+  locale: PropTypes.oneOf(['en', 'de']).isRequired,
+};
+
+Categories.defaultProps = {
+  activeCategoryId: null,
 };
 
 export default Categories;
