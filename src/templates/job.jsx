20e1d49f02416357fde336c00b9888d247d0c6bf
Alex Barashkov <lnikell@gmail.com>
2020-12-03 15:26:06 +0100

    fix: update gatsby wordpress plugin to support latest wp gatsby version

--- src/templates/job.jsx
+++ src/templates/job.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
 import { graphql } from 'gatsby';
 import React from 'react';
+import { Helmet } from 'react-helmet';
+import { useInView } from 'react-intersection-observer';
 
 import Content from 'components/pages/job/content';
 import Hero from 'components/pages/job/hero';
@@ -14,22 +16,55 @@ export default ({
     positions,
   },
   pageContext: { locale, pageUrls, menus, globalFields },
-}) => (
-  <MainLayout
-    seo={data.seo}
-    pageUrls={pageUrls}
-    menus={menus}
-    globalFields={globalFields}
-  >
-    <Hero title={data.title} locale={locale} />
-    <Content
-      content={data.content}
-      title={translations[locale].job.openPositionsTitle}
-      positions={positions}
-    />
-    <Contact locale={locale} />
-  </MainLayout>
-);
+}) => {
+  const [sectionRef, inView] = useInView({ triggerOnce: true, rootMargin: '500px' });
+
+  const handleOnLoad = () => {
+    hbspt.forms.create({
+      portalId: '7105834',
+      formId: 'af9cccae-d6a8-4aad-8daa-dc207893b330',
+      target: '#job-form-container',
+      locale: 'en',
+      inlineMessage: '',
+      onFormSubmitted: () => {
+        console.log('haha');
+      },
+    });
+  };
+  const handleScriptInject = ({ scriptTags }) => {
+    if (scriptTags) {
+      const scriptTag = scriptTags[0];
+      scriptTag.onload = handleOnLoad;
+    }
+  };
+
+  return (
+    <MainLayout
+      seo={data.seo}
+      pageUrls={pageUrls}
+      menus={menus}
+      globalFields={globalFields}
+    >
+      <Hero title={data.title} locale={locale} />
+      <Content
+        content={data.content}
+        title={translations[locale].job.openPositionsTitle}
+        positions={positions}
+      />
+      <section ref={sectionRef}>
+        <div className="container" id="job-form-container" />
+      </section>
+      {inView && (
+        <Helmet
+          script={[{ src: 'https://js.hsforms.net/forms/v2.js' }]}
+        // Helmet doesn't support `onload` in script objects so we have to hack in our own
+          onChangeClientState={(newState, addedTags) => handleScriptInject(addedTags)}
+        />
+      )}
+
+    </MainLayout>
+  );
+};
 
 export const query = graphql`
   query($id: String!, $locale: String!) {
