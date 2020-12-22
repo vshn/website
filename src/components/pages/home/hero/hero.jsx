7b5dc4f9c3c3e26711109c71b3ecd1c7e84ffd08
Vanny Le <vannnyle@gmail.com>
2020-12-22 17:50:36 +0200

    feat(components): add typing animation in Hero for Home page

--- src/components/pages/home/hero/hero.jsx
+++ src/components/pages/home/hero/hero.jsx
@@ -6,17 +6,17 @@ import { useInView } from 'react-intersection-observer';
 import Button from 'components/shared/button';
 import Heading from 'components/shared/heading';
 import useLottie from 'hooks/use-lottie';
-import getTextWithoutParagraph from 'utils/get-text-without-paragraph';
 
 import initialAnimationData from './data/initial-animation.json';
 import loopedAnimationData from './data/looped-animation.json';
 import styles from './hero.module.scss';
 import shape1 from './images/shape-1.svg';
 import shape2 from './images/shape-2.svg';
+import Typewritter from './typewritter';
 
 const cx = classNames.bind(styles);
 
-const Hero = ({ title, description, buttonLink: { url: buttonUrl, title: buttonText } }) => {
+const Hero = ({ title, animatedText, description, buttonLink: { url: buttonUrl, title: buttonText } }) => {
   const [animationPlayRef, isAnimationPlaying] = useInView();
 
   const [isInitialAnimationReady, setIsInitialAnimationReady] = useState(false);
@@ -72,10 +72,15 @@ const Hero = ({ title, description, buttonLink: { url: buttonUrl, title: buttonT
     ],
   );
 
+  const messages = animatedText.map((item) => item.text);
+
   return (
     <section className={cx('wrapper')}>
       <div className="container">
-        <Heading className={cx('title')} innerHTML={getTextWithoutParagraph(title)} />
+        <Heading className={cx('title')}>
+          {title}
+          <Typewritter messages={messages} />
+        </Heading>
         <p className={cx('description')}>{description}</p>
         <Button className={cx('button')} to={buttonUrl}>{buttonText}</Button>
 
@@ -100,11 +105,18 @@ const Hero = ({ title, description, buttonLink: { url: buttonUrl, title: buttonT
 
 Hero.propTypes = {
   title: PropTypes.string.isRequired,
+  animatedText: PropTypes.arrayOf(PropTypes.shape({
+    text: PropTypes.string,
+  })),
   description: PropTypes.string.isRequired,
   buttonLink: PropTypes.shape({
     url: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
   }).isRequired,
+};
+
+Hero.defaultProps = {
+  animatedText: [],
 };
 
 export default Hero;
