98d12e398f9b7c1373acbe64ea2ca55d2e02dbec
Vanny Le <vannnyle@gmail.com>
2021-01-27 14:05:57 +0200

    feat(lazy-blocks): add lazy block Hubspot Form to BlogPost

--- src/components/pages/blog-post/content/content.jsx
+++ src/components/pages/blog-post/content/content.jsx
@@ -2,17 +2,24 @@ import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import useHubspotForm from 'hooks/use-hubspot-form';
+
 import styles from './content.module.scss';
+
+import 'components/lazy-blocks/hubspot-form/hubspot-form.scss';
 
 const cx = classNames.bind(styles);
 
-const Content = ({ content }) => (
-  <article className={cx('wrapper')}>
-    <div className="container">
-      <div className={cx('content')} dangerouslySetInnerHTML={{ __html: content }} />
-    </div>
-  </article>
-);
+const Content = ({ content }) => {
+  useHubspotForm();
+  return (
+    <article className={cx('wrapper')}>
+      <div className="container">
+        <div className={cx('content')} dangerouslySetInnerHTML={{ __html: content }} />
+      </div>
+    </article>
+  );
+};
 
 Content.propTypes = {
   content: PropTypes.string.isRequired,
---
+++ src/hooks/use-hubspot-form.js
@@ -0,0 +1,40 @@
+import { useCallback, useEffect, useState } from 'react';
+
+const PORTAL_ID = '7105834';
+const FORM_JS_SRC = 'https://js.hsforms.net/forms/v2.js';
+
+export default function useForm() {
+  const [formId, setFormId] = useState(null);
+
+  const initForm = useCallback(() => {
+    if (!formId) {
+      return;
+    }
+    window.hbspt.forms.create({
+      portalId: PORTAL_ID,
+      formId,
+      target: '#form-container',
+    });
+  }, [formId]);
+
+  useEffect(() => {
+    if (formId) {
+      const existScript = document.head.querySelector(`script[src="${FORM_JS_SRC}"]`);
+      if (existScript) {
+        initForm();
+      } else {
+        const newScript = document.createElement('script');
+        newScript.onload = initForm;
+        newScript.src = FORM_JS_SRC;
+        document.head.appendChild(newScript);
+      }
+    }
+  }, [formId, initForm]);
+
+  useEffect(() => {
+    // Get form ID
+    const form = document.querySelector('.wp-block-lazyblock-hubspot-form');
+    const formIdData = form.querySelector('.hubspot-form').getAttribute('data-form-id');
+    setFormId(formIdData);
+  }, []);
+}
