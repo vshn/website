c9210fbd16a7819a3e9c3bab9a9dff308876c15b
Vanny Le <vannnyle@gmail.com>
2021-02-02 14:39:39 +0200

    chore: include Google Tag Manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -115,6 +115,12 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: 'gatsby-plugin-google-tagmanager',
+      options: {
+        id: 'GTM-5FC2LLB',
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
