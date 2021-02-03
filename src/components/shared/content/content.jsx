192c677783624b14ebfbd9e7b268a3c6841994d1
Vanny Le <vannnyle@gmail.com>
2021-02-03 16:03:17 +0200

    fix(components): move Content to shared components

--- src/components/shared/content/content.jsx
+++ src/components/shared/content/content.jsx
@@ -5,15 +5,18 @@ import React from 'react';
 import RelatedItems from 'components/shared/related-items';
 import 'components/lazy-blocks/subpage-cards/subpage-cards.scss';
 
+import Form from '../form';
+
 import styles from './content.module.scss';
 
 const cx = classNames.bind(styles);
 
-const Content = ({ content, relatedItems }) => (
+const Content = ({ content, relatedItems, formId }) => (
   <section className={cx('wrapper')}>
     <div className={cx('container', 'inner')}>
       <div className={cx('content')} dangerouslySetInnerHTML={{ __html: content }} />
       {relatedItems.items && <RelatedItems {...relatedItems} />}
+      {formId && <Form formId={formId} />}
     </div>
   </section>
 );
@@ -30,10 +33,12 @@ Content.propTypes = {
       }),
     })),
   }),
+  formId: PropTypes.string,
 };
 
 Content.defaultProps = {
   relatedItems: null,
+  formId: null,
 };
 
 export default Content;
