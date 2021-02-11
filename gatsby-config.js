2453750e62208bf3ab02d02f96f11ff4684674c2
Vanny Le <vannnyle@gmail.com>
2021-02-11 11:48:16 +0200

    chore: add sitemap generation

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
     'gatsby-plugin-netlify',
     {
       resolve: 'gatsby-source-filesystem',
