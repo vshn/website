7bda07d23b9258948f67fd288dde0e00fdd95926
Vanny Le <vannnyle@gmail.com>
2021-02-15 17:02:37 +0200

    feat(components): add description in Hero for Team page

--- src/components/shared/hero/hero.jsx
+++ src/components/shared/hero/hero.jsx
@@ -9,11 +9,12 @@ import styles from './hero.module.scss';
 
 const cx = classNames.bind(styles);
 
-const Hero = ({ breadcrumbs, title, pageTitle, backgroundImage }) => (
+const Hero = ({ breadcrumbs, title, pageTitle, description, backgroundImage }) => (
   <section className={cx('wrapper')}>
     <div className={cx('container', 'inner')}>
       {breadcrumbs?.length > 0 && <Breadcrumbs currentPageTitle={title} crumbs={breadcrumbs} />}
       <Heading className={cx('title')} tag="h1" size="xl">{pageTitle}</Heading>
+      {description && <p className={cx('description')}>{description}</p>}
     </div>
     <img className={cx('background-image')} src={backgroundImage} alt="" aria-hidden />
   </section>
@@ -24,11 +25,13 @@ Hero.propTypes = {
   })),
   title: PropTypes.string.isRequired,
   pageTitle: PropTypes.string.isRequired,
+  description: PropTypes.string,
   backgroundImage: PropTypes.string.isRequired,
 };
 
 Hero.defaultProps = {
   breadcrumbs: [],
+  description: '',
 };
 
 export default Hero;
