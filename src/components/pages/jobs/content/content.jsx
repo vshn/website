521340319d1d34552a0026e164a4a98ab0c22109
Vanny Le <vannnyle@gmail.com>
2021-07-27 09:47:20 +0300

    fix: update section Open Position

--- src/components/pages/jobs/content/content.jsx
+++ src/components/pages/jobs/content/content.jsx
@@ -2,6 +2,7 @@ import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { useInView } from 'react-intersection-observer';
 
 import styles from './content.module.scss';
 
@@ -10,16 +11,22 @@ import 'components/lazy-blocks/rating-cards/rating-cards.scss';
 const cx = classNames.bind(styles);
 const SRC_URL = 'https://s3.eu-central-1.amazonaws.com/files-eu.freshteam.com/production/84595/attachments/25000068981/original/25000001952_widget.js?1627285000';
 
-const Content = ({ content }) => (
-
-  <section className={cx('wrapper')}>
-    <Helmet script={[{ src: SRC_URL }]} />
-    <div className={cx('container', 'inner')}>
-      <div className={cx('widget')} id="freshteam-widget" />
-      <div className={cx('content')} dangerouslySetInnerHTML={{ __html: content }} />
-    </div>
-  </section>
-);
+const Content = ({ content }) => {
+  const [sectionRef, inView] = useInView({ triggerOnce: true });
+  return (
+    <section className={cx('wrapper')}>
+      <div ref={sectionRef} className={cx('container', 'inner')}>
+        <div className={cx('widget')} id="freshteam-widget" />
+        <div className={cx('content')} dangerouslySetInnerHTML={{ __html: content }} />
+      </div>
+      {inView && (
+      <Helmet
+        script={[{ src: SRC_URL }]}
+      />
+      )}
+    </section>
+  );
+};
 
 Content.propTypes = {
   content: PropTypes.string.isRequired,
