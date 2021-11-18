478366fd6551689dae4d734200557e893c7cf125
Vanny Le <vannnyle@gmail.com>
2021-11-18 18:16:55 +0300

    fix: ignore the conflict of css files order

--- gatsby-node.js
+++ gatsby-node.js
@@ -956,3 +956,33 @@ exports.createPages = async (args) => {
   // custom 404
   await createNotFound(params);
 };
+
+// workaround, which allows to ignore the conflict of css files order
+// https://github.com/gatsbyjs/gatsby/discussions/30169
+exports.onCreateWebpackConfig = ({ stage, actions, getConfig, plugins }) => {
+  const config = getConfig();
+  const miniCssExtractPluginIndex = config.plugins.findIndex(
+    (plugin) => plugin.constructor.name === 'MiniCssExtractPlugin',
+  );
+
+  if (miniCssExtractPluginIndex > -1) {
+    // remove miniCssExtractPlugin from plugins list
+    config.plugins.splice(miniCssExtractPluginIndex, 1);
+
+    // re-add mini-css-extract-plugin
+    if (stage === 'build-javascript') {
+      config.plugins.push(plugins.extractText({
+        filename: '[name].[contenthash].css',
+        chunkFilename: '[name].[contenthash].css',
+        ignoreOrder: true,
+      }));
+    } else {
+      config.plugins.push(plugins.extractText({
+        filename: '[name].css',
+        chunkFilename: '[id].css',
+        ignoreOrder: true,
+      }));
+    }
+  }
+  actions.replaceWebpackConfig(config);
+};
