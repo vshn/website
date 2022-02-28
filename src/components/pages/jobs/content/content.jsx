8109c9f5cc207afa4b58529a1c6d90fa5a06486a
Vanny Le <vannnyle@gmail.com>
2022-02-28 13:40:25 +0200

    feat: integrate recruitee jobs widget

--- src/components/pages/jobs/content/content.jsx
+++ src/components/pages/jobs/content/content.jsx
@@ -1,8 +1,8 @@
 import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
-import React from 'react';
-import { Helmet } from 'react-helmet';
-import { useInView } from 'react-intersection-observer';
+import React, { useEffect } from 'react';
+
+import Heading from 'components/shared/heading';
 
 import RatingCards from '../rating-cards';
 
@@ -11,23 +11,52 @@ import styles from './content.module.scss';
 import 'components/lazy-blocks/rating-cards/rating-cards.scss';
 
 const cx = classNames.bind(styles);
-const SRC_URL = 'https://s3.eu-central-1.amazonaws.com/files-eu.freshteam.com/production/84595/attachments/25000068981/original/25000001952_widget.js?1627285000';
 
 const Content = ({ content, ratingCards, ratingCards2 }) => {
-  const [sectionRef, inView] = useInView({ triggerOnce: true });
+  useEffect(() => {
+    const rtscript = document.createElement('script');
+    rtscript.type = 'text/javascript';
+    rtscript.onload = () => {
+      if (window.RTWidget) {
+        const { RTWidget } = window;
+        window.widget = new RTWidget({
+          companies: [],
+          detailsMode: 'popup',
+          language: 'en',
+          departmentsFilter: [],
+          themeVars: {
+            primary: '#1999e3',
+            secondary: '#000',
+            text: '#5c6f78',
+            textDark: '#37474f',
+            fontFamily: '"Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif;',
+            baseFontSize: '16px',
+          },
+          flags: {
+            showLocation: true,
+            showCountry: true,
+            showCity: true,
+            groupByLocation: false,
+            groupByDepartment: true,
+            groupByCompany: false,
+          },
+        });
+      }
+    };
+    rtscript.src = 'https://d10zminp1cyta8.cloudfront.net/widget.js';
+    document.body.appendChild(rtscript);
+  }, []);
+
   return (
     <section className={cx('wrapper')}>
-      <div ref={sectionRef} className={cx('container', 'inner')}>
-        <div className={cx('widget')} id="freshteam-widget" />
+      <div className={cx('container', 'inner')}>
+        <Heading className={cx('title')} tag="h2" size="xl">Open positions</Heading>
+        <div className={cx('recruitee')} id="recruitee-careers" />
         <div className={cx('content')} dangerouslySetInnerHTML={{ __html: content }} />
         <RatingCards {...ratingCards} />
         <RatingCards {...ratingCards2} />
       </div>
-      {inView && (
-      <Helmet
-        script={[{ src: SRC_URL }]}
-      />
-      )}
+
     </section>
   );
 };
